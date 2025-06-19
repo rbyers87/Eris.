1eae7e0ed8535f1d72e7c70a05ca9a6e85b43b90
Natxo <natxetepala@gmail.com>
2025-06-19 09:17:37 +0200

    Initial Commit

---
+++ Eris./Eris_App.swift
@@ -0,0 +1,17 @@
+//
+//  Eris_App.swift
+//  Eris.
+//
+//  Created by Ignacio Palacio  on 19/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Eris_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
